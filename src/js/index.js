17f03d029a5ad44c491f3f18104b2ff7bc9a3e3f
Jarek Janowski <jarjanowski95@gmail.com>
2020-06-11 21:31:03 +0200

    added js interaction to buttons

--- src/js/index.js
+++ src/js/index.js
@@ -2,4 +2,32 @@ import '../scss/main.scss';
 
 /* place your code below */
 
-console.log('HELLO 🚀')
+const entry = localStorage.getItem('entry');
+let result = '';
+
+// if (entry) {
+//     result = entry;
+// }
+
+const entryInput = document.querySelector('.form__textarea--js');
+entryInput.value = result;
+
+
+
+const saveButton = document.querySelector('.button--save-js');
+const loadButton = document.querySelector('.button--load-js');
+const clearButton = document.querySelector('.button--clear-js')
+
+saveButton.addEventListener('click', () => {
+    localStorage.setItem('entry', entryInput.value);
+    entryInput.value = '';
+})
+
+loadButton.addEventListener('click', () => {
+    localStorage.getItem('entry', entryInput.value);
+    entryInput.value = entry;
+})
+
+clearButton.addEventListener('click', () => {
+    localStorage.clear();
+})
